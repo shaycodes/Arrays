16d853a2a37075fe6e2dc99091fd98fb1a65d231
Shay Lindsey <shaycodes@gmail.com>
2022-10-18 20:43:44 -0700

    Add files via upload
    Array Practice

---
+++ src/MorePractice.java
@@ -0,0 +1,14 @@
+public class MorePractice {
+    public static void main(String[] args) {
+        double[] exampleArray = {1, 5, 6, 5, 4, 1};
+        double maximum = exampleArray[0];
+        int index = 0;
+        for (int i = 1; i < exampleArray.length; i++) {
+            if (exampleArray[i] > maximum) {
+                maximum = exampleArray[i];
+                index = i;
+            }
+        }
+        System.out.println(index);
+    }
+}
---
+++ src/Practice.java
@@ -0,0 +1,15 @@
+public class Practice {
+    public static void main(String []args){
+
+        int arr[] = {5,10,15,20,25,30};
+        int sum=0;
+
+        for(int i=0;i<arr.length;i++){
+            sum += arr[i];
+        }
+
+        System.out.println("Sum of array is: "+sum);
+
+    }
+}
+
